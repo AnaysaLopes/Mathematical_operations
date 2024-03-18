1ca1faddb8437e78f57078d0c0f0d4e08a7e9387
Anaysa Lopes <153683976+AnaysaLopes@users.noreply.github.com>
2024-03-18 12:54:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+alert("Hora do cálculo!")
+
+let firstNumber = prompt("Digite o primeiro número:")
+let secondNumber = prompt("Digite o segundo número:")
+
+firstNumber = Number(firstNumber)
+secondNumber = Number(secondNumber)
+
+
+let sum = firstNumber + secondNumber
+
+let sub = firstNumber - secondNumber
+
+let multi = firstNumber * secondNumber
+
+let div = (firstNumber / secondNumber).toFixed(2)
+
+let restDiv = firstNumber % secondNumber
+
+
+
+alert('Soma: ' + sum)
+
+if (sum % 2 == 0){
+    alert('A soma dos dois números é par')
+} else{
+    alert('A soma dos dois números é ímpar')
+}
+
+
+alert('Subtração: ' + sub)
+
+alert('Multiplicação: ' + multi)
+
+alert('Divisão: ' + div)
+
+alert('Resto da divisão: ' + restDiv)
+
+
+if (firstNumber != secondNumber){
+    alert('Os números inseridos são diferentes')
+} else{
+    alert('Os números inseridos são iguais')
+}
